2e10afd1ecaefa7479460ce4cb31db1d02e816cd
Fynn Forster <fforster@1-B-12.42heilbronn.de>
2025-02-14 18:10:02 +0100

    no dogs allowed, only WOLVES

--- helloworld.c
+++ helloworld.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   helloworld.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nsloniow <nsloniow@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fforster <fforster@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 17:45:40 by fforster          #+#    #+#             */
-/*   Updated: 2025/02/14 17:56:52 by nsloniow         ###   ########.fr       */
+/*   Updated: 2025/02/14 18:09:36 by fforster         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 int	main(void)
 {
 	printf("Cute wild dark wolve cubs rule the world!\n");
+	printf("at least they aren't modern dogs\n");
 	return (0);
 }
